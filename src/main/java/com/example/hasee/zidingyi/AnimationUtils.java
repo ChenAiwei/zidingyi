e55e3fb9209c6bcc309e4a4bafd02265cdabc3d5
ChenAiwei <2268967541@qq.com>
2016-05-09 00:08:12 +0800

    完善,点击菜单按钮实现全部动画菜单的开启或关闭，修复了动画在执行时不可二次点击的功能

--- src/main/java/com/example/hasee/zidingyi/AnimationUtils.java
+++ src/main/java/com/example/hasee/zidingyi/AnimationUtils.java
@@ -1,6 +1,7 @@
 package com.example.hasee.zidingyi;
 
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 
@@ -8,30 +9,54 @@ import android.widget.RelativeLayout;
  * Created by hasee on 2016/5/8.
  */
 public class AnimationUtils {
+    public static int animationCount = 0;
+
     //开始动画
-    public  static void start(RelativeLayout rl){
-        for (int i=0;i<rl.getChildCount();i++) {
+    public static void start(RelativeLayout rl, int startofset) {
+        for (int i = 0; i < rl.getChildCount(); i++) {
             rl.getChildAt(i).setEnabled(true);
         }
-       RotateAnimation animation=new RotateAnimation(-180,0,
-               RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,1);
-        animation.setDuration(500);
-        animation.setFillAfter(true);
-
-        rl.startAnimation(animation);
-    }
-    //结束动画
-    public static void close(RelativeLayout rl,int startofset){
-        for (int i=0;i<rl.getChildCount();i++) {
-            rl.getChildAt(i).setEnabled(false);
-        }
-        RotateAnimation animation=new RotateAnimation(0,-180,
-                RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,1);
+        RotateAnimation animation = new RotateAnimation(-180, 0,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1);
         animation.setDuration(500);
         //设置带延时效果的动画
         animation.setStartOffset(startofset);
         animation.setFillAfter(true);
-
+        animation.setAnimationListener(new MyanimationListner());
         rl.startAnimation(animation);
+    }
+
+    //结束动画
+    public static void close(RelativeLayout rl, int startofset) {
+        for (int i = 0; i < rl.getChildCount(); i++) {
+            rl.getChildAt(i).setEnabled(false);
+        }
+        RotateAnimation animation = new RotateAnimation(0, -180,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1);
+        animation.setDuration(500);
+        //设置带延时效果的动画
+        animation.setStartOffset(startofset);
+        animation.setFillAfter(true);
+        animation.setAnimationListener(new MyanimationListner());
+        rl.startAnimation(animation);
+    }
+
+    static class MyanimationListner implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            animationCount++;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            animationCount--;
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
--- src/main/java/com/example/hasee/zidingyi/MainActivity.java
+++ src/main/java/com/example/hasee/zidingyi/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hasee.zidingyi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RelativeLayout leve1;
     private RelativeLayout leve2;
     private RelativeLayout leve3;
-    private boolean isleve2 = true, isleve3 = true;
+    private boolean isleve2 = true, isleve3 = true, ismenu = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,33 +44,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_leve1_home:
-                int startofset=0;
+
+
+                if (AnimationUtils.animationCount != 0) {
+                    return;
+                }
+                int startofset = 0;
                 if (isleve2) {
 
                     //同时关闭三级菜单
-                    if(isleve3){
-                        AnimationUtils.close(leve3,startofset);
-                        startofset+=200;
-                        isleve3=false;
+                    if (isleve3) {
+                        AnimationUtils.close(leve3, startofset);
+                        startofset += 200;
+                        isleve3 = false;
                     }
                     //关闭
-                    AnimationUtils.close(leve2,startofset);
+                    AnimationUtils.close(leve2, startofset);
                 } else {
                     //显示
-                    AnimationUtils.start(leve2);
+                    AnimationUtils.start(leve2, 0);
                 }
                 isleve2 = !isleve2;
                 break;
             case R.id.iv_leve2_menu:
+                if (AnimationUtils.animationCount != 0) {
+                    return;
+                }
                 if (isleve3) {
                     //关闭
-                    AnimationUtils.close(leve3,0);
+                    AnimationUtils.close(leve3, 0);
                 } else {
                     //显示
-                    AnimationUtils.start(leve3);
+                    AnimationUtils.start(leve3, 0);
                 }
                 isleve3 = !isleve3;
                 break;
         }
+    }
+    //点击菜单按键把菜单动画全部关闭或隐藏
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            if (ismenu) {
+                int startofset = 0;
+                if (isleve3) {
+                    AnimationUtils.close(leve3, startofset);
+                    startofset += 200;
+                }
+                if (isleve2) {
+                    AnimationUtils.close(leve2, startofset);
+                    startofset += 200;
+                }
+                AnimationUtils.close(leve1, startofset);
+                ismenu = false;
+            } else {
+                //显示所有菜单
+                int startofset = 0;
+                AnimationUtils.start(leve1, startofset);
+                startofset += 200;
+                AnimationUtils.start(leve2, startofset);
+                startofset += 200;
+                AnimationUtils.start(leve3, startofset);
+                ismenu = true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
